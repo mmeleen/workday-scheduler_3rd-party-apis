d28117efb4530222c3cdf0b39bf32b1725007530
Matt Meleen <mmeleen@utexas.edu>
2020-06-24 18:38:51 -0500

    First 3 inputs working; Date & hour with moment.js

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-var fullDate = new Date();
-var twoDigitMonth = fullDate.getMonth() + ""; if (twoDigitMonth.length == 1) twoDigitMonth = "0" + twoDigitMonth;
-var twoDigitDate = fullDate.getDate() + ""; if (twoDigitDate.length == 1) twoDigitDate = "0" + twoDigitDate;
-var currentDate = twoDigitMonth + "/" + twoDigitDate + "/" + fullDate.getFullYear();
-$("#currentDay").text(currentDate);
+// Get current date and apply to header
+$("#currentDay").text(moment().format('MMMM Do, YYYY'));
 
+
+// ---- Variable Declarations & localStorage Value Retrieval ----
+
+var currentHour = moment().hour();
+console.log(currentHour);
+
+// 9AM
 var nineLock = $("#nine-lock");
 var nineText = $("#nine-text");
 nineText.val(localStorage.getItem("nineEntry"));
 
+// 10AM
+var tenLock = $("#ten-lock");
+var tenText = $("#ten-text");
+tenText.val(localStorage.getItem("tenEntry"));
+
+// 11AM
+var elevenLock = $("#eleven-lock");
+var elevenText = $("#eleven-text");
+elevenText.val(localStorage.getItem("elevenEntry"));
+
+
+// ---- Lock Button Clicks & localStorage Saves ----
+
+// 9AM
 nineLock.on("click", function() {
   console.log(nineText.val());
   var nineEntry = nineText.val();
   localStorage.setItem("nineEntry", nineEntry);
+});
+
+// 10AM
+tenLock.on("click", function() {
+  console.log(tenText.val());
+  var tenEntry = tenText.val();
+  localStorage.setItem("tenEntry", tenEntry);
+});
+
+// 11AM
+elevenLock.on("click", function() {
+  console.log(elevenText.val());
+  var elevenEntry = elevenText.val();
+  localStorage.setItem("elevenEntry", elevenEntry);
 });
